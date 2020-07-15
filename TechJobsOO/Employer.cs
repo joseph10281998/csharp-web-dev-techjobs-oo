0c43ccc277f56529d4095f083700abf7148fdfb2
josephPark <josephpark1028@gmail.com>
2020-07-14 20:43:30 -0500

    Complete

--- TechJobsOO/Employer.cs
+++ TechJobsOO/Employer.cs
@@ -1,46 +1,50 @@
 ﻿using System;
 namespace TechJobsOO
 {
-    public class Employer
+    public class Employer : JobField
     {
-        public int Id { get; }
-        private static int nextId = 1;
-        public string Value { get; set; }    //name of the employer
-
-        /// <summary>
-        /// use: var e = new Employer();
-        /// e.Value = "Equifax";
-        /// or var e = new Employer { Value = "Equifax" };
-        /// </summary>
-        public Employer()  //constructor
-        {
-            Id = nextId;
-            nextId++;
-        }
-        //tally -> there could many instances and nextId is continuously incremented and assigned to the intances
-        /// <summary>
-        /// use: var e = new Employer("Equifax");
-        /// </summary>
-        /// <param name="value"></param> - parameter
-        public Employer(string value) : this()    //constructor
-        {
-            Value = value;
+        public Employer(string value) : base(value)
+        {            
         }
 
-        public override bool Equals(object obj)     //obj consists of employer's name and id(?)
-        {
-            return (obj is Employer employer) &&
-                   (Id == employer.Id);
-        }
+        //public int Id { get; }
+        //private static int nextId = 1;
+        //public string Value { get; set; }    //name of the employer
 
-        public override int GetHashCode()
-        {
-            return HashCode.Combine(Id);
-        }
+        ///// <summary>
+        ///// use: var e = new Employer();
+        ///// e.Value = "Equifax";
+        ///// or var e = new Employer { Value = "Equifax" };
+        ///// </summary>
+        //public Employer()  //constructor
+        //{
+        //    Id = nextId;
+        //    nextId++;
+        //}
+        ////tally -> there could many instances and nextId is continuously incremented and assigned to the intances
+        ///// <summary>
+        ///// use: var e = new Employer("Equifax");
+        ///// </summary>
+        ///// <param name="value"></param> - parameter
+        //public Employer(string value) : this()    //constructor
+        //{
+        //    Value = value;
+        //}
 
-        public override string ToString()
-        {
-            return Value;
-        }
+        //public override bool Equals(object obj)     //obj consists of employer's name and id(?)
+        //{
+        //    return (obj is Employer employer) &&
+        //           (Id == employer.Id);
+        //}
+
+        //public override int GetHashCode()
+        //{
+        //    return HashCode.Combine(Id);
+        //}
+
+        //public override string ToString()
+        //{
+        //    return Value;
+        //}
     }
 }
